5eb4dba9b7960d2a1e49cdeb185f971483c2c8ed
Andrea Mengoli <andrea.mengoli@ynap.com>
2019-01-07 16:46:12 +0100

    Add Lugaresi protocol

---
+++ Classes/Slang.swift
@@ -0,0 +1,12 @@
+//
+//  Slang.swift
+//  
+//
+//  Created by Andrea Mengoli on 1/7/19.
+//
+
+/// All classes that implements this protocol are able to call self like
+/// the famous Lugaresi.
+protocol Lugaresi {
+    typealias scelf = Self
+}
